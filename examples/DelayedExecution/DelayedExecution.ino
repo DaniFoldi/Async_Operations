91a101ab1f3514193149fd47b172cf19e5fdf2cb
Dani Foldi <dani@stefan.hu>
2020-09-02 17:11:56 +0200

    Fixed and renamed examples

---
+++ examples/DelayedExecution/DelayedExecution.ino
@@ -0,0 +1,18 @@
+#import <Async_Operations.h>
+
+long long dt = {1000};
+Async_Operations delayed(dt, 1, 1);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  delayed.setLoopCallback(&cb);
+  delayed.start();
+}
+
+void cb() {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+  delayed.update();
+}

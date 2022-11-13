9d533f124d9bd37c0590b9cf58c3f98f013a5bf0
DarioGHub <49622669+DarioGHub@users.noreply.github.com>
2022-11-13 16:32:15 -0700

    Update DelayedExecution.ino
    import -> include, add step_count calc

--- examples/DelayedExecution/DelayedExecution.ino
+++ examples/DelayedExecution/DelayedExecution.ino
@@ -1,7 +1,8 @@
-#import <Async_Operations.h>
+#include <Async_Operations.h>
 
-long long dt = {1000};
-Async_Operations delayed(&dt, 1, 1);
+long long dt[] = {1000};
+int step_count = sizeof(dt)/sizeof(dt[0]);
+Async_Operations delayed(dt, step_count, 1);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
